37a4faf2bc92b419e57e8bcf98857ffff1b52d85
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-08-11 01:43:31 +0200

    parse metro results

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var request = require('request');
+var cheerio = require('cheerio');
 
 app.get('/', function(req, res){
-  res.send('Hello world');
+  url = 'http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/mairie+de+clichy/13/A'
+  request(url, function(error, response, html) {
+    if (error) {
+      res.send(error);
+    }
+    else {
+      var schedule = Array();
+      var $ = cheerio.load(html);
+      $('#prochains_passages tbody tr').filter(function(){
+        $(this).each(function() {
+          var destination = $(this).children().first().text();
+          var arriving = $(this).children().last().text();
+          schedule.push({'destination' : destination, 'arriving' : arriving});
+        });
+      });
+      res.send(schedule);
+    }
+    
+  });
+  
 });
 
 http.listen(process.env.PORT || 3000, function(){
